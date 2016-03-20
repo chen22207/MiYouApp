47bcdefd637f1b4caf3132fefec0568ce9a33d15
Protocol <1014988429@qq.com>
2016-03-20 21:21:07 +0800

    。。。

---
+++ app/src/main/java/com/firstblood/miyo/application/MainApplication.java
@@ -0,0 +1,13 @@
+package com.firstblood.miyo.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/3/19.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
