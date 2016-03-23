b0b3a8534728ead81e0a53a6bac12251e4cd3adf
chenshuai12619 <1014988429@qq.com>
2016-03-23 20:23:18 +0800

    登录界面

--- app/src/main/java/com/firstblood/miyo/activity/LoginActivity.java
+++ app/src/main/java/com/firstblood/miyo/activity/LoginActivity.java
@@ -1,68 +1,19 @@
 package com.firstblood.miyo.activity;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.firstblood.miyou.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
-
-    @InjectView(R.id.login_vp)
-    ViewPager loginVp;
-
-    private List<ImageView> images = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
-        for (int i = 0; i < 5; i++) {
-            ImageView iv = new ImageView(this);
-//            iv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100));
-//            iv.setScaleType(ImageView.ScaleType.FIT_XY);
-            iv.setImageResource(R.drawable.f1);
-            images.add(iv);
-        }
-
-        loginVp.setAdapter(new MyPagerAdapter());
-
-    }
-
-    private class MyPagerAdapter extends PagerAdapter {
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(images.get(position));
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(images.get(position));
-            return images.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return images.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
 
     }
 }
