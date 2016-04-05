397c95aa7f4faac01d55d7d56435944ae0586a9b
chenshuai12619 <1014988429@qq.com>
2016-04-05 17:25:49 +0800

    个人中心

--- app/src/main/java/com/firstblood/miyo/activity/MainActivity.java
+++ app/src/main/java/com/firstblood/miyo/activity/MainActivity.java
@@ -1,89 +1,101 @@
 package com.firstblood.miyo.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.firstblood.miyo.R;
 import com.firstblood.miyo.fragment.HomePageFragment;
-import com.jakewharton.rxbinding.widget.RxRadioGroup;
 
 import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @InjectView(R.id.main_fl)
-    FrameLayout mainFl;
+	@InjectView(R.id.main_fl)
+	FrameLayout mainFl;
+	@InjectView(R.id.main_tab_home_ib)
+	ImageButton mMainTabHomeIb;
+	@InjectView(R.id.main_tab_collection_ib)
+	ImageButton mMainTabCollectionIb;
+	@InjectView(R.id.main_tab_search_ib)
+	ImageButton mMainTabSearchIb;
+	@InjectView(R.id.main_tab_message_ib)
+	ImageButton mMainTabMessageIb;
+	@InjectView(R.id.main_tab_mine_ib)
+	ImageButton mMainTabMineIb;
 
-    private FragmentManager fragmentManager;
+	private FragmentManager fragmentManager;
 
-    private ArrayList<Fragment> fragmentList = new ArrayList<>();
-    private String[] FRAGMENT_TAG = {"首页", "收藏", "消息", "我"};
+	private ArrayList<Fragment> fragmentList = new ArrayList<>();
+	private String[] FRAGMENT_TAG = {"首页", "收藏", "搜索", "消息", "我"};
+	private String currentTag = "";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ButterKnife.inject(this);
 
-        fragmentManager = getSupportFragmentManager();
-        initEvent();
+		fragmentManager = getSupportFragmentManager();
+		initEvent();
 
 
-    }
+	}
 
-    private void initEvent() {
-        RxRadioGroup.checkedChanges(mainRg).subscribe(integer -> {
-            hideAllFragment();
-            switch (integer) {
-                case R.id.main_tab_home_rb:
-                    Fragment f = fragmentManager.findFragmentByTag(FRAGMENT_TAG[0]);
-                    if (f != null) {
-                        fragmentManager.beginTransaction().show(f).commit();
-                    } else {
-                        HomePageFragment homePageFragment = HomePageFragment.newInstance();
-                        fragmentList.add(homePageFragment);
-                        fragmentManager.beginTransaction().add(R.id.main_fl, homePageFragment, FRAGMENT_TAG[0]).commit();
-                    }
-                    break;
-                case R.id.main_1_rb:
-                    Fragment f1 = fragmentManager.findFragmentByTag(FRAGMENT_TAG[1]);
-                    if (f1 != null) {
-                        fragmentManager.beginTransaction().show(f1).commit();
-                    } else {
-                        HomePageFragment homePageFragment = HomePageFragment.newInstance();
-                        fragmentList.add(homePageFragment);
-                        fragmentManager.beginTransaction().add(R.id.main_fl, homePageFragment, FRAGMENT_TAG[1]).commit();
-                    }
-                    break;
-                case R.id.main_2_rb:
-                    Fragment f2 = fragmentManager.findFragmentByTag(FRAGMENT_TAG[2]);
-                    if (f2 != null) {
-                        fragmentManager.beginTransaction().show(f2).commit();
-                    } else {
-                        HomePageFragment homePageFragment = HomePageFragment.newInstance();
-                        fragmentList.add(homePageFragment);
-                        fragmentManager.beginTransaction().add(R.id.main_fl, homePageFragment, FRAGMENT_TAG[2]).commit();
-                    }
-                    break;
-                case R.id.main_tab_mine_rb:
-                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                    break;
-            }
-        });
+	private void initEvent() {
+		mMainTabHomeIb.setOnClickListener(this);
+		mMainTabCollectionIb.setOnClickListener(this);
+		mMainTabSearchIb.setOnClickListener(this);
+		mMainTabMessageIb.setOnClickListener(this);
+		mMainTabMineIb.setOnClickListener(this);
+	}
 
-    }
+	private void hideAllFragment() {
+		for (Fragment fragment : fragmentList) {
+			fragmentManager.beginTransaction().hide(fragment).commit();
+		}
+	}
 
-    private void hideAllFragment() {
-        for (Fragment fragment : fragmentList) {
-            fragmentManager.beginTransaction().hide(fragment).commit();
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		hideAllFragment();
+		if (v == mMainTabHomeIb) {
+			if (currentTag.equals(FRAGMENT_TAG[0])) {
+				return;
+			} else {
+				currentTag = FRAGMENT_TAG[0];
+			}
+			Fragment f = fragmentManager.findFragmentByTag(FRAGMENT_TAG[0]);
+			if (f != null) {
+				fragmentManager.beginTransaction().show(f).commit();
+			} else {
+				HomePageFragment homePageFragment = HomePageFragment.newInstance();
+				fragmentList.add(homePageFragment);
+				fragmentManager.beginTransaction().add(R.id.main_fl, homePageFragment, FRAGMENT_TAG[0]).commit();
+			}
+		} else if (v == mMainTabCollectionIb) {
+			if (currentTag.equals(FRAGMENT_TAG[1])) {
+				return;
+			} else {
+				currentTag = FRAGMENT_TAG[1];
+			}
+			Fragment f1 = fragmentManager.findFragmentByTag(FRAGMENT_TAG[1]);
+			if (f1 != null) {
+				fragmentManager.beginTransaction().show(f1).commit();
+			} else {
+				HomePageFragment homePageFragment = HomePageFragment.newInstance();
+				fragmentList.add(homePageFragment);
+				fragmentManager.beginTransaction().add(R.id.main_fl, homePageFragment, FRAGMENT_TAG[1]).commit();
+			}
+		} else if (v == mMainTabSearchIb) {
 
+		}
+	}
 }
---
+++ app/src/main/java/com/firstblood/miyo/fragment/MineFragment.java
@@ -0,0 +1,19 @@
+package com.firstblood.miyo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chenshuai12619 on 2016/4/5 10:01.
+ */
+public class MineFragment extends Fragment {
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
