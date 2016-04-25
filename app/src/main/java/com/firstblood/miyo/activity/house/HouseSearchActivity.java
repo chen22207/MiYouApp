4af12fec14fdaed379f21f109b1eab74808d47ab
cs-mac <1014988429@qq.com>
2016-04-25 22:29:59 +0800

    搜索界面-popupwindow

--- app/src/main/java/com/firstblood/miyo/activity/house/HouseSearchActivity.java
+++ app/src/main/java/com/firstblood/miyo/activity/house/HouseSearchActivity.java
@@ -1,12 +1,16 @@
 package com.firstblood.miyo.activity.house;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.firstblood.miyo.R;
@@ -14,6 +18,7 @@ import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class HouseSearchActivity extends AppCompatActivity {
 
@@ -27,6 +32,12 @@ public class HouseSearchActivity extends AppCompatActivity {
 	TextView mSearchFilterTv;
 	@InjectView(R.id.search_xrv)
 	XRecyclerView mSearchXrv;
+
+	private enum SearchSort {
+		PUBLISH, HITS, PRICE_DESC, PRICE_ASC
+	}
+
+	private SearchSort currentSort = SearchSort.PUBLISH;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,4 +57,71 @@ public class HouseSearchActivity extends AppCompatActivity {
 		SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
 		return true;
 	}
+
+
+	@OnClick({R.id.search_main_order_tv, R.id.search_location_order_tv, R.id.search_filter_tv})
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.search_main_order_tv:
+				showMainOrderPopupwindow();
+				break;
+			case R.id.search_location_order_tv:
+				break;
+			case R.id.search_filter_tv:
+				break;
+		}
+	}
+
+	private PopupWindow sortPw;
+
+	private void showMainOrderPopupwindow() {
+		sortPw = new PopupWindow(initSortView(), ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		sortPw.showAsDropDown(mSearchMainOrderTv);
+	}
+
+	private View initSortView() {
+		View v = View.inflate(this, R.layout.popup_search_main_order, null);
+		TextView publishTimeTv = (TextView) v.findViewById(R.id.search_order_publish_time_tv);
+		TextView hitsTv = (TextView) v.findViewById(R.id.search_order_hits_tv);
+		TextView priceDescTv = (TextView) v.findViewById(R.id.search_order_price_desc_tv);
+		TextView priceAscTv = (TextView) v.findViewById(R.id.search_order_price_asc_tv);
+		publishTimeTv.setOnClickListener(mMainOrderItemClickListener);
+		hitsTv.setOnClickListener(mMainOrderItemClickListener);
+		priceDescTv.setOnClickListener(mMainOrderItemClickListener);
+		priceAscTv.setOnClickListener(mMainOrderItemClickListener);
+		final int color = ContextCompat.getColor(this, R.color.colorPrimary);
+		switch (currentSort) {
+			case PUBLISH:
+				publishTimeTv.setTextColor(color);
+				break;
+			case HITS:
+				hitsTv.setTextColor(color);
+				break;
+			case PRICE_DESC:
+				priceDescTv.setTextColor(color);
+				break;
+			case PRICE_ASC:
+				priceAscTv.setTextColor(color);
+				break;
+		}
+		return v;
+	}
+
+	private View.OnClickListener mMainOrderItemClickListener = v -> {
+		switch (v.getId()) {
+			case R.id.search_order_publish_time_tv:
+				currentSort = SearchSort.PUBLISH;
+				break;
+			case R.id.search_order_hits_tv:
+				currentSort = SearchSort.HITS;
+				break;
+			case R.id.search_order_price_desc_tv:
+				currentSort = SearchSort.PRICE_DESC;
+				break;
+			case R.id.search_order_price_asc_tv:
+				currentSort = SearchSort.PRICE_ASC;
+				break;
+		}
+		sortPw.dismiss();
+	};
 }
