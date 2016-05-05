a9ce042cf922295fa605757823aac81c48d1a10f
cs-mac <1014988429@qq.com>
2016-05-05 15:09:28 +0800

    地图

--- app/src/main/java/com/firstblood/miyo/activity/MapActivity.java
+++ app/src/main/java/com/firstblood/miyo/activity/MapActivity.java
@@ -1,8 +1,20 @@
 package com.firstblood.miyo.activity;
 
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -12,12 +24,22 @@ import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.Projection;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.CameraPosition;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
 import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.firstblood.miyo.R;
 import com.firstblood.miyo.util.AlertMessageUtil;
+import com.orhanobut.logger.Logger;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -26,32 +48,58 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
 
 	@InjectView(R.id.map_view)
 	MapView mMapView;
+	@InjectView(R.id.map_back_iv)
+	ImageView mMapBackIv;
+	@InjectView(R.id.map_sv)
+	SearchView mMapSv;
+	@InjectView(R.id.map_rv)
+	RecyclerView mMapRv;
 
 	private AMapLocationClient locationClient;
 	private AMapLocationClientOption locationClientOption;
 	private OnLocationChangedListener locationChangedListener;
 	private Marker centerMarker;
+	private AMap aMap;
+	private ArrayList<PoiItem> aroundPois;
+	private MyAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		ButterKnife.inject(this);
-		//地图
+		mMapBackIv.setOnClickListener(v -> finish());
+		aroundPois = new ArrayList<>();
 		mMapView.onCreate(savedInstanceState);
-		AMap map = mMapView.getMap();
-		map.setLocationSource(this);
-		map.getUiSettings().setMyLocationButtonEnabled(true);
-		map.getUiSettings().setScaleControlsEnabled(true);
-		map.setMyLocationEnabled(true);
+		initMap();
+		LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+		layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+		mMapRv.setLayoutManager(layoutManager);
+		adapter = new MyAdapter();
+		mMapRv.setAdapter(adapter);
+	}
+
+	private void initMap() {
+		//地图
+		aMap = mMapView.getMap();
+		aMap.setLocationSource(this);
+		aMap.getUiSettings().setMyLocationButtonEnabled(true);
+		aMap.getUiSettings().setScaleControlsEnabled(true);
+		aMap.setMyLocationEnabled(true);
+		aMap.setOnCameraChangeListener(this);
 		MyLocationStyle locationStyle = new MyLocationStyle();
 		locationStyle.strokeColor(Color.BLACK);
 		locationStyle.strokeWidth(1.0f);
-		map.setMyLocationStyle(locationStyle);
+		aMap.setMyLocationStyle(locationStyle);
 
 		//移动到杭州
-		map.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(30.285384, 120.154606)));
-
+		aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(30.285384, 120.154606), 13));
+		LatLng target = aMap.getCameraPosition().target;
+		MarkerOptions markerOptions = new MarkerOptions();
+		markerOptions.position(target);
+		markerOptions.draggable(true);
+		markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_map_location));
+		centerMarker = aMap.addMarker(markerOptions);
 	}
 
 	@Override
@@ -87,7 +135,7 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
 			if (aMapLocation.getErrorCode() == 0) {
 				locationChangedListener.onLocationChanged(aMapLocation);
 			} else {
-				AlertMessageUtil.showAlert(this, "定位失败");
+				AlertMessageUtil.showAlert(this, "定位失败" + aMapLocation.getErrorInfo() + "--" + aMapLocation.getLocationDetail());
 			}
 		}
 	}
@@ -117,11 +165,101 @@ public class MapActivity extends AppCompatActivity implements LocationSource, AM
 
 	@Override
 	public void onCameraChange(CameraPosition cameraPosition) {
-
+		centerMarker.setPosition(cameraPosition.target);
 	}
 
 	@Override
 	public void onCameraChangeFinish(CameraPosition cameraPosition) {
+		jumpPoint(centerMarker, cameraPosition.target);
+		searchAround(cameraPosition.target);
+	}
 
+	public void jumpPoint(final Marker marker, LatLng latLng) {
+		final Handler handler = new Handler();
+		final long start = SystemClock.uptimeMillis();
+		Projection proj = aMap.getProjection();
+		Point startPoint = proj.toScreenLocation(latLng);
+		startPoint.offset(0, -50);
+		final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+		final long duration = 1200;
+
+		final Interpolator interpolator = new BounceInterpolator();
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				long elapsed = SystemClock.uptimeMillis() - start;
+				float t = interpolator.getInterpolation((float) elapsed
+						/ duration);
+				double lng = t * latLng.longitude + (1 - t)
+						* startLatLng.longitude;
+				double lat = t * latLng.latitude + (1 - t)
+						* startLatLng.latitude;
+				marker.setPosition(new LatLng(lat, lng));
+				aMap.invalidate();// 刷新地图
+				if (t < 1.0) {
+					handler.postDelayed(this, 16);
+				}
+			}
+		});
+	}
+
+	private void searchAround(LatLng latLng) {
+		PoiSearch.Query query = new PoiSearch.Query("", "", "");
+		query.setPageSize(10);
+		query.setPageNum(1);
+		PoiSearch search = new PoiSearch(this, query);
+		search.setBound(new PoiSearch.SearchBound(new LatLonPoint(latLng.latitude, latLng.longitude), 1000));
+		search.setOnPoiSearchListener(new PoiSearch.OnPoiSearchListener() {
+			@Override
+			public void onPoiSearched(PoiResult poiResult, int i) {
+				if (i == 1000) {
+					ArrayList<PoiItem> pois = poiResult.getPois();
+					aroundPois.clear();
+					aroundPois.addAll(pois);
+					adapter.notifyDataSetChanged();
+				} else {
+					Logger.e("搜周边错误：" + i);
+				}
+			}
+
+			@Override
+			public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+			}
+		});
+		search.searchPOIAsyn();
+	}
+
+	private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View v = View.inflate(parent.getContext(), R.layout.listitem_map_poi, null);
+			return new ViewHolder(v);
+		}
+
+		@Override
+		public void onBindViewHolder(ViewHolder holder, int position) {
+			ViewHolder h = holder;
+			PoiItem item = aroundPois.get(position);
+			h.title.setText(item.getAdName());
+			h.description.setText(item.getTitle());
+		}
+
+		@Override
+		public int getItemCount() {
+			return aroundPois.size();
+		}
+
+		class ViewHolder extends RecyclerView.ViewHolder {
+			private TextView title;
+			private TextView description;
+
+			public ViewHolder(View itemView) {
+				super(itemView);
+				title = (TextView) itemView.findViewById(R.id.listitem_map_poi_title_tv);
+				description = (TextView) itemView.findViewById(R.id.listitem_map_poi_des_tv);
+			}
+		}
 	}
 }
--- app/src/main/java/com/firstblood/miyo/util/CommonUtils.java
+++ app/src/main/java/com/firstblood/miyo/util/CommonUtils.java
@@ -1,6 +1,8 @@
 package com.firstblood.miyo.util;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.widget.ImageView;
@@ -11,6 +13,9 @@ import com.isseiaoki.simplecropview.util.Utils;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 import me.nereo.multi_image_selector.MultiImageSelectorFragment;
 import rx.Observable;
@@ -44,5 +49,38 @@ public class CommonUtils {
 				imageView.setImageResource(R.drawable.icon_default_head_img);
 			}
 		}
+	}
+
+	/**
+	 * 打印SHA1
+	 *
+	 * @param context
+	 * @return
+	 */
+	public static String sHA1(Context context) {
+		try {
+			PackageInfo info = context.getPackageManager().getPackageInfo(
+					context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+			byte[] cert = info.signatures[0].toByteArray();
+
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+			byte[] publicKey = md.digest(cert);
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < publicKey.length; i++) {
+				String appendString = Integer.toHexString(0xFF & publicKey[i])
+						.toUpperCase(Locale.US);
+				if (appendString.length() == 1)
+					hexString.append("0");
+				hexString.append(appendString);
+				hexString.append(":");
+			}
+			return hexString.toString();
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
