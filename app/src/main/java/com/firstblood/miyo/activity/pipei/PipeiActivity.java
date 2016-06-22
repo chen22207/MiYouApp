88a4bed47be9d7e0164b8160d8fdece577b620e1
cs-mac <1014988429@qq.com>
2016-06-22 09:58:19 +0800

    ...

---
+++ app/src/main/java/com/firstblood/miyo/activity/pipei/PipeiActivity.java
@@ -0,0 +1,15 @@
+package com.firstblood.miyo.activity.pipei;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firstblood.miyo.R;
+
+public class PipeiActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pipei);
+	}
+}
